b2ada43742d5e46c4102ecff64899f5335c82d2a
Adrian Gruber <adrian_gruber@epam.com>
2020-02-29 14:57:24 +0100

    Adjust Todo shcema

--- models/Todo.ts
+++ models/Todo.ts
@@ -1,35 +1,5 @@
 import mongoose from 'mongoose';
 
-const AssigneeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Assignee name is required'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    validate: {
-      validator: (email: string) => {
-        const emailRegex = /\S+@\S+\.\S+/;
-        return emailRegex.test(email);
-      },
-      message: (props) => `${props} is an invalid email`,
-    },
-  },
-});
-
-const SubTaskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, 'Sub-task title is required'],
-    minlength: [5, 'Title should at least five character'],
-  },
-  completed: {
-    type: Boolean,
-  },
-});
-
 const TodoSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -43,17 +13,43 @@ const TodoSchema = new mongoose.Schema({
   priority: {
     type: String,
     enum: ['High', 'Normal', 'Low'],
+    required: [true, 'Priority is required'],
   },
   deadline: {
     type: String,
     required: [true, 'Deadline is required'],
   },
   assignee: {
-    type: AssigneeSchema,
+    name: {
+      type: String,
+      required: [true, 'Assignee name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: {
+        validator: (email: string) => {
+          const emailRegex = /\S+@\S+\.\S+/;
+          return emailRegex.test(email);
+        },
+        message: (props: any) => `${props.value} is an invalid email`,
+      },
+    },
   },
-  subTasks: {
-    type: SubTaskSchema,
-  },
+  subTasks: [
+    {
+      title: {
+        type: String,
+        trim: true,
+        required: [true, 'Sub-task title is required'],
+        minlength: [5, 'Title should at least five character'],
+      },
+      completed: {
+        type: Boolean,
+        required: [true, 'Sub-task completed is required'],
+      },
+    },
+  ],
 });
 
 const todoModel = mongoose.model('Todo', TodoSchema);
